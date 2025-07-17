81805c35c50683d3d80f1f651d1dbd88ae5056db
Norman <norman@wamberchies.be>
2025-07-17 18:21:40 +0200

    Bismuth added

--- src/main/java/net/norman/normanaddon/NormanAddon.java
+++ src/main/java/net/norman/normanaddon/NormanAddon.java
@@ -1,5 +1,7 @@
 package net.norman.normanaddon;
 
+import net.minecraft.world.item.CreativeModeTabs;
+import net.norman.normanaddon.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -34,6 +36,8 @@ public class NormanAddon {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
@@ -46,7 +50,9 @@ public class NormanAddon {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.BISMUTH);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
